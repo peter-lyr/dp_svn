a2a69d8a285c9a70d37768f1bac356c866b443a8
peter-lyr <llydrp.ldp@gmail.com>
2024-07-19 21:22:19 +0800

    function M.tortoisesvn(cmd, cur, prompt)

--- lua/dp_svn.lua
+++ lua/dp_svn.lua
@@ -10,40 +10,65 @@ if B.check_plugins {
   return
 end
 
-local function tortoisesvn(params)
-  if not params or #params < 3 then return end
-  local cmd, cmd1, cmd2, root, yes = unpack(params)
-  if #params == 3 then
-    cmd, root, yes = unpack(params)
-  elseif #params == 4 then
-    cmd1, cmd2, root, yes = unpack(params)
-    cmd = cmd1 .. ' ' .. cmd2
+-- 帮助
+-- {https://blog.csdn.net/kenkao/article/details/103384392}
+
+function M.tortoisesvn(cmd, cur, prompt)
+  if not cmd then
+    return
   end
-  if not cmd then return end
-  local abspath = (root == 'root') and B.get_proj_root() or B.buf_get_name()
-  if yes == 'yes' or vim.tbl_contains({ 'y', 'Y', }, vim.fn.trim(vim.fn.input('Sure to update? [Y/n]: ', 'Y'))) == true then
-    vim.fn.execute(string.format('silent !%s && start tortoiseproc.exe /command:%s /path:\"%s\"', B.system_cd(abspath), cmd, abspath))
+  local path = B.get_proj_root()
+  if cur == 'cur' then
+    path = B.buf_get_name()
+  end
+  if not prompt or B.is_sure('Sure to %s in %s', cmd, path) then
+    vim.fn.execute(string.format('silent !%s && start tortoiseproc.exe /command:%s /path:\"%s\"', B.system_cd(path), cmd, path))
   end
 end
 
-vim.api.nvim_create_user_command('TortoiseSVN', function(params)
-  tortoisesvn(params['fargs'])
+vim.api.nvim_create_user_command('TortoiseSvn', function(params)
+  M.tortoisesvn(params['fargs'])
 end, { nargs = '*', })
 
 require 'which-key'.register {
   ['<leader>v'] = { name = 'dp_svn', },
-  ['<leader>vu'] = { '<cmd>TortoiseSVN update /rev root yes<cr>', 'svn update /rev root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vf'] = { '<cmd>TortoiseSVN diff root yes<cr>', 'svn diff root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>v;'] = { '<cmd>TortoiseSVN log root yes<cr>', 'svn log root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vo'] = { '<cmd>TortoiseSVN settings cur yes<cr>', 'svn settings cur yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vd'] = { '<cmd>TortoiseSVN diff cur yes<cr>', 'svn diff cur yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vb'] = { '<cmd>TortoiseSVN blame cur yes<cr>', 'svn blame cur yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vw'] = { '<cmd>TortoiseSVN repobrowser cur yes<cr>', 'svn repobrowser cur yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>ve'] = { '<cmd>TortoiseSVN repobrowser root yes<cr>', 'svn repobrowser root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vv'] = { '<cmd>TortoiseSVN revert root yes<cr>', 'svn revert root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>va'] = { '<cmd>TortoiseSVN add root yes<cr>', 'svn add root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vc'] = { '<cmd>TortoiseSVN commit root yes<cr>', 'svn commit root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vl'] = { '<cmd>TortoiseSVN log cur yes<cr>', 'svn log cur yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>vk'] = { '<cmd>TortoiseSVN checkout root yes<cr>', 'svn checkout root yes<cr>', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>v<a-c>'] = { '<cmd>TortoiseSVN cleanup root yes<cr>', 'svn cleanup root yes<cr>', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vo'] = { function() M.tortoisesvn('settings', 'cur') end, 'svn: settings cur', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>vf'] = { function() M.tortoisesvn 'diff' end, 'svn: diff', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vd'] = { function() M.tortoisesvn('diff', 'cur') end, 'svn: diff cur', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>v;'] = { function() M.tortoisesvn 'log' end, 'svn: log', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vl'] = { function() M.tortoisesvn('log', 'cur') end, 'svn: log cur', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>vw'] = { function() M.tortoisesvn('repobrowser', 'cur') end, 'svn: repobrowser cur', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>ve'] = { function() M.tortoisesvn 'repobrowser' end, 'svn: repobrowser', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vu'] = { function() M.tortoisesvn 'update /rev' end, 'svn: update /rev', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>vb'] = { function() M.tortoisesvn('blame', 'cur') end, 'svn: blame cur', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>vv'] = { function() M.tortoisesvn 'revert' end, 'svn: revert', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>vk'] = { function() M.tortoisesvn 'checkout' end, 'svn: checkout', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vn'] = { function() M.tortoisesvn 'cleanup' end, 'svn: cleanup', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>va'] = { function() M.tortoisesvn 'add' end, 'svn: add', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>vc'] = { function() M.tortoisesvn 'commit' end, 'svn: commit', mode = { 'n', 'v', }, silent = true, },
+}
+
+require 'which-key'.register {
+  ['<leader>v<leader>'] = { function() vim.cmd [[call feedkeys(":\<c-u>TortoiseSvn ")]] end, 'svn: more', mode = { 'n', 'v', }, },
 }
